12fcb46ffbd48f948b85ab22aa0e226940236474
prettyturtle <unifconti@gmail.com>
2023-05-13 00:13:19 +0900

    Initial Commit

---
+++ Swift_30_Projects_Interests/Swift_30_Projects_InterestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_30_Projects_InterestsApp.swift
+//  Swift_30_Projects_Interests
+//
+//  Created by yc on 2023/05/13.
+//
+
+import SwiftUI
+
+@main
+struct Swift_30_Projects_InterestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
